5c27f650c4a2d8055ee20b90c5539fff2e217784
Adyb Fadel <adyb.fadel@gmail.com>
2015-08-24 23:13:31 -0300

    remove etc

--- etc/del-duplic-apont.sql
+++
@@ -1,8 +0,0 @@
-delete from ponto where id in (select max(p.id) as id from ponto p
-group by p.funcionario_id, p.apontamento_id
-having count(1) > 1
-order by p.apontamento_id)
-
-delete from apontamento where id in (select max(a.id) as id from Apontamento a
-group by a.data, a.obra_id
-HAVING count(1) > 1 and a.obra_id is not null)
